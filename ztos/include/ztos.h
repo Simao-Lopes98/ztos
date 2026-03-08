9c47aa43204c9df9de785c772ba4021a0e879bb8
simao.lopes98 <simaolopes98@gmail.com>
2026-03-08 16:53:26 +0000

    Add starting point functions on ztos

--- ztos/include/ztos.h
+++ ztos/include/ztos.h
@@ -3,14 +3,37 @@
 #define ZTOS_H
 
 /* Includes */
-#include <stdint.h>
-#include <stddef.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include <stm32f1xx.h>
+#include <string.h>
+#include "clock.h"
+#include "app.h"
 
 /* Defines */
 
+#define MAX_TASK_NAME 16
+
 /* Type definitions */
 
+/* Task Function pointer */
+typedef void (* zTaskHandler) ();
+
+typedef enum
+{
+    TASK_RUNNING,
+    TASK_STOPPED
+}zTaskStatus_t;
+
+typedef struct
+{
+    void * stackPtr;
+    uint32_t stackSize;
+    char name [MAX_TASK_NAME];
+    zTaskHandler entryFn;
+    zTaskStatus_t status;
+    uint64_t ticks;
+} zTask_t;
+ 
 /* Private variables */
 
 /* Private function prototypes */
--- ztos/src/ztos.c
+++ ztos/src/ztos.c
@@ -6,12 +6,41 @@
 /* Includes */
 #include "ztos.h"
 
-/* Defines */
-
-/* Type definitions */
-
 /* Private variables */
 
 /* Private function prototypes */
 
 /* Public functions */
+STATUS zSchedInit ()
+{
+    /* Start Timer for tick */
+
+    /* Create SLL for task nodes */
+
+    /* Create Idle Task */
+
+
+}
+
+STATUS zTaskCreate (char * name,
+                    uint32_t stackSize,
+                    zTaskHandler taskEntryFn)
+{
+    if (name == NULL)
+    {
+        return ERROR;
+    }
+
+    if (taskEntryFn == NULL)
+    {
+        return ERROR;
+    }
+
+    /* Allocate stack size */
+
+    /* Fabricate entry point */
+
+    /* Insert task on list node */
+    
+    return OK;
+}
